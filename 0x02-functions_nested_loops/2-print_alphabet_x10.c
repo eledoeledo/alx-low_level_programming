672e09ba5593e3ea2e5fb22276ad6e4cf9138095
eledoeledo <eledoeledo89@gmail.com>
2022-10-19 04:41:04 -0700

    toto

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*print_alphabet_x10 - prints 10 times the alphabet, in lowercase
+*is written by eledo
+*/
+
+void print_alphabet_x10(void)
+{
+	int n;
+
+	char m;
+
+	for (n = 0; n < 10; n++)
+	{
+		for (m = 'a'; m <= 'z'; m++)
+		{
+			_putchar(m);
+		}
+	}
+	_putchar('\n');
+}
