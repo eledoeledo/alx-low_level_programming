673553f92e300010608a3b6ff7b2791701076e2e
eledoeledo <eledoeledo89@gmail.com>
2022-10-19 12:28:07 -0700

    toto

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,7 @@
 /**
 *print_sign - Write a function that prints the sign of a number
 *@n: check value of n
-*1 and prints + if n is greater than zero
+*Return: 1 and prints + if n is greater than zero
 *0 and prints 0 if n is zero
 *-1 and prints - if n is less than zero
 */
