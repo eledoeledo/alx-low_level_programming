cd37a56f71ffcd41881ed6e2ffcabf95b8666962
eledoeledo <eledoeledo89@gmail.com>
2022-10-19 04:06:24 -0700

    papa

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+*print_alphabet - function that print alphabet lowercase
+*is written by eledo
+*/
+
+void print_alphabet(void)
+{
+	char lolo;
+	for (lolo = 'a'; lolo <= 'z'; lolo++)
+	{
+		_putchar(lolo);
+	}
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,6 @@
 #ifndef MAIN_FILE
 #define MAIN_FILE
 int _putchar(char c);
+void print_alphabet(void);
 
 #endif
