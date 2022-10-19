0c89a3e02a1cba6a3ac1fdca71f19ed2003bda18
eledoeledo <eledoeledo89@gmail.com>
2022-10-19 12:24:33 -0700

    toto

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -3,7 +3,9 @@
 /**
 *print_sign - Write a function that prints the sign of a number
 *@n: check value of n
-*Return: 1 and prints + if n is greater than zero 0 and prints 0 if n is zero -1 and prints - if n is less than zero
+*1 and prints + if n is greater than zero
+*0 and prints 0 if n is zero
+*-1 and prints - if n is less than zero
 */
 
 int print_sign(int n)
@@ -13,14 +15,14 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-	else if (n < 0)
+	else if (n == 0)
 	{
-		_putchar('-');
-		return (-1);
+		_putchar('0');
+		return (0);
 	}
 	else
 	{
-		_putchar(0);
-		return (0);
+		_putchar('-');
+		return (-1);
 	}
 }
