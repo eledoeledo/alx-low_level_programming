3ac85a519759ec5ad6f4690f1b696ec505022294
eledoeledo <eledoeledo89@gmail.com>
2022-10-20 04:41:55 -0700

    papa

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*add - function that adds two integer
+*@n:first integer
+*@j:second integer
+*Return: result of add
+*/
+
+int add(int n, int j)
+{
+	int r = n + j;
+
+	return (r);
+}
