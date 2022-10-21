d29d058778b81987d56b3920854e2cb0adde2981
eledoeledo <eledoeledo89@gmail.com>
2022-10-21 09:45:02 -0700

    papa

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+*print_triangle - Write a function that prints a triangle
+@size:is the size of the triangle
+*/
+
+void print_triangle(int size)
+{
+	int i = 1, ii;
+
+	while (i <= size && size > 0)
+	{
+		ii = 0;
+		while (ii < size - i)
+		{
+			_putchar(' ');
+			ii++;
+		}
+		ii = 0;
+		while (ii < i)
+		{
+			_putchar('#');
+			ii++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	if (i == 1)
+	{
+		_putchar('\n');
+	}
+}
