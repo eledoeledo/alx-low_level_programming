eef55c58be40517342b5ea3492f97e97f4cf653e
eledoeledo <eledoeledo89@gmail.com>
2022-10-21 01:23:13 -0700

    toto

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+*mul(int a, int b) - function that multiplies two integers
+*@a:integer first
+*@b:integer two
+*Return: Always 0 (Success)
+*/
+
+int mul(int a, int b)
+{
+	int toto = a * b;
+
+	return (0);
+}
