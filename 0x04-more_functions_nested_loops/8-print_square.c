29eeffcc37cb24191acdfef4966a299ca17939e7
eledoeledo <eledoeledo89@gmail.com>
2022-10-21 08:59:51 -0700

    polo

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+*print_square - function that prints a square
+*@size:number
+*/
+
+void print_square(int size)
+{
+	int i = 0, ii;
+
+	while (i < size && size > 0)
+	{
+		ii = 0;
+		while (ii < size)
+		{
+			_putchar('#');
+			ii++;
+		}
+		_putchar('\n');
+		i++;
+	}
+	if (i == 0)
+	{
+		_putchar('\n');
+	}
+}
