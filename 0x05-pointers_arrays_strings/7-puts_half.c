05072e0ac04df0ce0a3be55c98380b6c1196cdee
eledoeledo <eledoeledo89@gmail.com>
2022-10-24 08:26:26 -0700

    lola

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+*puts_half - Write a function that prints half of a string
+*@str:char
+*/
+
+void puts_half(char *str)
+{
+	int i = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
