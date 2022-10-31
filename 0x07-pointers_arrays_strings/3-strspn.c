9c2810423660b98dda0f39d281effdfddd8747f7
eledoeledo <eledoeledo89@gmail.com>
2022-10-31 10:01:48 -0700

    lolo

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+*int _strspn - Write a function that gets the length of a prefix substring
+*@s:Char
+*@accept:Char
+*Return: the number of bytes in the initial segment of s which
+*consist only of bytes from accept
+*/
+
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int bytes = 0;
+
+	int index;
+
+	while (*s)
+	{
+		for (index = 0; accept[index]; index++)
+		{
+			if (*s == accept[index])
+			{
+				bytes++;
+				break;
+			}
+			else if (accept[index + 1] == '\0')
+			{
+				return (bytes);
+			}
+		}
+		s++;
+	}
+	return (bytes);
+}
