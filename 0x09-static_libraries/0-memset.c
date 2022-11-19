71a6614661794d1bdf22b049ad106b0379cfbbfc
eledoeledo <eledoeledo89@gmail.com>
2022-11-19 02:21:32 -0800

    lola

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,21 +1,21 @@
 #include "main.h"
 
 /**
-**_memset - Write a function that fills memory with a constant byte
-*@s:Char
-*@b:Char
-*@n:Int
-*Return: s
+*_memset - fill a block of memory with a specific value
+*@s:starting address of memory to be filled
+*@b:the desired value
+*@n:number of bytes to be changed
+*Return: changed array with new value for n bytes
 */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i;
+	int i = 0;
 
-	for (i = 0; n > 0; i++, n--)
+	for (; n > 0; i++)
 	{
 		s[i] = b;
+		n--;
 	}
 	return (s);
 }
-
